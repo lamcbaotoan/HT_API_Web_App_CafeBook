14de8201fc91d52f1236bb42d462d8251bb5583b
lamcbaotoan <lamchubaotoan@gmail.com>
2025-11-15 19:00:38 +0700

    bak

---
+++ TaiLieu/backup.sql
@@ -0,0 +1,5 @@
+BACKUP DATABASE [CAFEBOOKDB_v2]
+TO DISK = N'E:\Tai Lieu Hoc Tap\CS 445 AM\Project\TaiLieu\code\CAFEBOOKDB_v2.bak'
+WITH FORMAT,
+     INIT,
+     NAME = N'Backup_Full_CAFEBOOKDB_v2';
